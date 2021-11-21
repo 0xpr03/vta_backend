5f4d00637ae9d97c2df74b6a27567c692c827525
Aron Heinecke <aron.heinecke@t-online.de>
2021-11-21 18:30:04 +0100

    rework ecdsa test script

--- testing/ecdsa.py
+++ testing/ecdsa.py
@@ -1,3 +1,4 @@
+from typing import AnyStr, Dict
 import jwt
 import requests
 import uuid
@@ -7,33 +8,63 @@ url = "http://localhost:8080"
 #header_json = {"Content-Type": "application/json", "Accept" : "application/json"}
 
 
-server_info = requests.get(url+"/api/v1/server/info").json()
-print(server_info)
 
 pubkey = "-----BEGIN PUBLIC KEY-----\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEEVs/o5+uQbTjL3chynL4wXgUg2R9\nq9UU8I5mEovUf86QZ7kOBIjJwqnzD1omageEHWwHdBO6B+dFabmdT9POxg==\n-----END PUBLIC KEY-----"
 privkey = "-----BEGIN PRIVATE KEY-----\nMIGHAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBG0wawIBAQQgevZzL1gdAFr88hb2\nOF/2NxApJCzGCEDdfSp6VQO30hyhRANCAAQRWz+jn65BtOMvdyHKcvjBeBSDZH2r\n1RTwjmYSi9R/zpBnuQ4EiMnCqfMPWiZqB4QdbAd0E7oH50VpuZ1P087G\n-----END PRIVATE KEY-----"
-payload = {
-    #"aud": [server_info["id"]],
-    "aud": ["612b4cd3-87c4-4c81-abdd-f7cecadbcef0"],
-    "nbf": int( time()-4 ),
-    "iat": int( time() ),
-    "exp": int( time()+5 ),
-    "sub": "register",
-    "iss": str(uuid.uuid4()),
-    "name": "toaster",
-    "delete_after": int( time()+60*60 )
-    }
-print("send payload: "+str(payload))
-encoded = jwt.encode(payload,key= privkey, algorithm="ES256")
-print("encoded: "+str(encoded))
-print("send header: "+str(jwt.get_unverified_header(encoded)))
-# decoded = jwt.decode(encoded, pubkey, algorithms=["ES256"])
-# print(decoded)
 
+def server_info():
+    return requests.get(url+"/api/v1/server/info").json()
 
-payload = {"key": pubkey, "proof": encoded, "keytype": "EC_PEM"}
-res = requests.post(url+"/api/v1/account/register",json=payload)
-print(res)
-print(res.url)
-print(res.text)
-print(res.headers)
+server = server_info()
+user_id = uuid.uuid4()
+
+def jwt_payload(server,user_id,sub) -> Dict[str, AnyStr]:
+    return {
+        "aud": [server["id"]],
+        #"aud": ["612b4cd3-87c4-4c81-abdd-f7cecadbcef0"],
+        "nbf": int( time()-4 ),
+        "iat": int( time() ),
+        "exp": int( time()+5 ),
+        "sub": sub,
+        "iss": str(user_id),
+        "name": "toaster",
+        "delete_after": int( time()+60*60 )
+        }
+
+def register(server, pubkey, privkey, user_id):
+    payload = jwt_payload(server,user_id, "register")
+    payload["name"] = "toaster"
+    payload["delete_after"] = int( time()+60*60 )
+    print("send payload: "+str(payload))
+    encoded = jwt.encode(payload,key= privkey, algorithm="ES256")
+    print("encoded: "+str(encoded))
+    print("send header: "+str(jwt.get_unverified_header(encoded)))
+
+    payload = {"key": pubkey, "proof": encoded, "keytype": "EC_PEM"}
+    res = requests.post(url+"/api/v1/account/register",json=payload)
+    if res.status_code != 202:
+        print(res)
+        print(res.url)
+        print(res.text)
+        print(res.headers)
+        raise Exception(f"Register failed with {res.status_code}")
+
+def login(server, privkey, user_id):
+    payload = jwt_payload(server,user_id, "login")
+    encoded = jwt.encode(payload,key= privkey, algorithm="ES256")
+
+    payload = {"iss": str(user_id), "proof": encoded}
+    res = requests.post(url+"/api/v1/account/login/key",json=payload)
+    if res.status_code != 200:
+        print(res)
+        raise Exception(f"Login failed with {res.status_code}")
+    return res.cookies
+
+def account_info(cookies):
+    requests.get(url+"/api/v1/account/info",cookies=cookies).json()
+
+print("registering")
+register(server, pubkey, privkey, user_id)
+print("logging in")
+cookies = login(server, privkey, user_id)
+print(account_info(cookies))
